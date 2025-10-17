c515d0eb22ca882ab27f1b54922a70300e221d62
Manish Dait <daitmanish88@gmail.com>
2025-10-17 20:05:26 +0530

    feat:added checksum to file_id
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/file/file_id.py
+++ src/hiero_sdk_python/file/file_id.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 
 from hiero_sdk_python.hapi.services import basic_types_pb2
+from hiero_sdk_python.utils.entity_id_helper import (
+    parse_from_string,
+    validate_checksum,
+    format_to_string_with_checksum
+)
 
 
 @dataclass
@@ -19,6 +24,7 @@ class FileId:
     shard: int = 0
     realm: int = 0
     file: int = 0
+    checksum: str | None = None
     
     @classmethod
     def _from_proto(cls, file_id_proto: basic_types_pb2.FileID) -> 'FileId':
@@ -46,14 +52,36 @@ class FileId:
         """
         Creates a FileId instance from a string in the format 'shard.realm.file'.
         """
-        parts = file_id_str.strip().split('.')
-        if len(parts) != 3:
-            raise ValueError("Invalid file ID string format. Expected 'shard.realm.file'")
-        shard, realm, file = map(int, parts)
-        return cls(shard, realm, file)
+        shard, realm, file, checksum = parse_from_string(file_id_str)
+        file_id: FileId = cls(shard, realm, file)
+        object.__setattr__(file_id, 'checksum', checksum)
+
+        return file_id
     
     def __str__(self) -> str:
         """
         Returns a string representation of the FileId instance.
         """
         return f"{self.shard}.{self.realm}.{self.file}"
+    
+    def validate_checksum(self, client) -> None:
+        """Validate the checksum for the FileId instance"""
+        validate_checksum(
+            shard=self.shard,
+            realm=self.realm,
+            num=self.num,
+            checksum=self.checksum,
+            client=client
+        )
+
+    def to_string_with_checksum(self, client) -> str:
+        """
+        Returns the string representation of the FileId with checksum 
+        in the format 'shard.realm.num-checksum'
+        """
+        return format_to_string_with_checksum(
+            shard=self.shard,
+            realm=self.realm,
+            num=self.num,
+            client=client
+        )
