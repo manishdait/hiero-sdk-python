f060927269f3b0b0949329a3d85a9dde6318b260
Manish Dait <daitmanish88@gmail.com>
2026-02-08 12:44:26 +0530

    chore: fix typo
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/account/account_records_query.py
+++ src/hiero_sdk_python/account/account_records_query.py
@@ -108,7 +108,7 @@ class AccountRecordsQuery(Query):
 
         Args:
             client (Client): The client instance to use for execution
-            timeout (Optional[Union[int, float]): The total execution timeout (in seconds) for this execution.
+            timeout (Optional[Union[int, float]]): The total execution timeout (in seconds) for this execution.
 
         Returns:
             List[TransactionRecord]: The account records from the network
