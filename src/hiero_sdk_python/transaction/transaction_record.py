6415d40064226e182f5a2fcc43875ebd5b440542
Manish Dait <daitmanish88@gmail.com>
2026-02-13 14:23:02 +0530

    chore: fix unused imports and prints
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/transaction/transaction_record.py
+++ src/hiero_sdk_python/transaction/transaction_record.py
@@ -252,7 +252,6 @@ class TransactionRecord:
         elif entropy_case == "prng_bytes":
             prng_bytes = proto.prng_bytes
 
-        proto.automatic_token_associations
         return cls(
             transaction_id=tx_id,
             transaction_hash=proto.transactionHash,
