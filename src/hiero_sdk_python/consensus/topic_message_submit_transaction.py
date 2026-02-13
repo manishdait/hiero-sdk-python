6bb866deb575cd99e2b765b9f3ee472c7832d3fa
Manish Dait <daitmanish88@gmail.com>
2026-02-13 14:44:28 +0530

    chore: converted method to property
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- src/hiero_sdk_python/consensus/topic_message_submit_transaction.py
+++ src/hiero_sdk_python/consensus/topic_message_submit_transaction.py
@@ -423,13 +423,10 @@ class TopicMessageSubmitTransaction(Transaction):
         super().sign(private_key)
         return self
     
-    def get_body_size_all_chunks(self) -> List[int]:
+    @property
+    def body_size_all_chunks(self) -> List[int]:
         """
         Returns an array of body sizes for transactions with multiple chunks.
-
-        Returns:
-            List[int]: An array of body sizes, where each element represents the 
-                size in bytes of a chunk's transaction body
         """
         self._require_frozen()
         sizes = []
@@ -437,7 +434,7 @@ class TopicMessageSubmitTransaction(Transaction):
         original_index = self._current_index
         for transaction_id in self._transaction_ids:
             self.transaction_id = transaction_id
-            sizes.append(self.get_body_size())
+            sizes.append(self.body_size)
         
         self._current_index = original_index
         return sizes
